b0fcf57f01cc92c759aeed758af1c221ceeacf12
alina <kostenkoalina97@icloud.com>
2025-08-24 13:13:39 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listElements = document.querySelectorAll('li');
+
+function parseSalary(salary) {
+  return Number(salary.replace('$', '').replace(',', ''));
+}
+
+function sortedList(items) {
+  const sortedBySalary = Array.from(items).sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  sortedBySalary.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(items) {
+  const employees = [];
+
+  items.forEach((item) => {
+    const employee = {
+      name: item.textContent,
+      position: item.dataset.position,
+      salary: parseSalary(item.dataset.salary),
+      age: item.dataset.age,
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+sortedList(listElements);
+getEmployees(listElements);
